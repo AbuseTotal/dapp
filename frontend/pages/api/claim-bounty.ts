eb4b327f08a79f0006a45c630148d30b670a9a8c
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2023-03-18 06:44:09 +0000

    endpoints fixes

--- frontend/pages/api/claim-bounty.ts
+++ frontend/pages/api/claim-bounty.ts
@@ -4,23 +4,18 @@ import BountyContractAbi from '@/abis/Bounty.json';
 import { AbiItem } from 'web3-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { userId, submittedAdress } = req.body;
+  const { userId, submittedAddress = '0x976EA74026E726554dB657fA54763abd0C3a0aa9' } = req.body;
 
-  const address =
-    submittedAdress ?? '0xe9ff693c5c7ab05ec827330e5cb0555733eac166ca7ca4cdd96f8f06fd7d3ae0';
+  const web3 = await new Web3('http://localhost:8545');
 
-  const web3 = new Web3('http://localhost:8545');
-
-  console.log('IS ADDRESS: ', web3.utils.isAddress(address));
-
-  const bountyContract = new web3.eth.Contract(
+  const bountyContract = await new web3.eth.Contract(
     BountyContractAbi as AbiItem[],
     process.env.NEXT_PUBLIC_BOUNTY_CONTRACT_ADDRESS,
   );
 
-  console.log('IS ADDRESS: ', web3.utils.isAddress(address));
-
-  bountyContract.methods.claimBounty(address, userId).send({ from: process.env.NEXT_PUBLIC_OWNER });
+  await bountyContract.methods
+    .claimBounty(submittedAddress, userId)
+    .send({ from: process.env.NEXT_PUBLIC_OWNER });
 
   res.status(200).send({});
 }
--- frontend/pages/api/update-reputation.ts
+++ frontend/pages/api/update-reputation.ts
@@ -1,24 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Web3 from 'web3';
-import BountyContractAbi from '@/abis/Bounty.json';
+import ReputationContractAbi from '@/abis/Bounty.json';
 import { AbiItem } from 'web3-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const {
-    userId,
-    submittedAdress = process.env.NEXT_PUBLIC_EXAMPLE_ADDRESS,
-    bountyValue,
-  } = req.body;
+  const { userId, submittedAdress = process.env.NEXT_PUBLIC_EXAMPLE_ADDRESS } = req.body;
 
   const web3 = new Web3('http://localhost:8545');
 
   const bountyContract = new web3.eth.Contract(
-    BountyContractAbi as AbiItem[],
-    process.env.NEXT_PUBLIC_BOUNTY_CONTRACT_ADDRESS,
+    ReputationContractAbi as AbiItem[],
+    process.env.NEXT_PUBLIC_REPUTATION_CONTRACT_ADDRESS,
   );
 
   bountyContract.methods
-    .submitBounty(bountyValue, userId)
+    .claimBounty(submittedAdress, userId)
     .send({ from: process.env.NEXT_PUBLIC_OWNER });
 
   res.status(200).send({});
